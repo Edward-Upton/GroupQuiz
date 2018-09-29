7cb40c4f1c67eecedca57956d0d34b87c219383c
mitgobla <ben-dodd@outlook.com>
2018-09-29 11:07:43 +0100

    Animated Slider Fix

--- UI/lighting_desk.py
+++ UI/lighting_desk.py
@@ -5,7 +5,7 @@ from rtmidi.midiutil import open_midioutput
 from rtmidi.midiconstants import CONTROL_CHANGE
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QStyleFactory
-
+from math import floor
 from Ui_lighting_control import Ui_MainWindow
 
 APP = QApplication([])
@@ -85,75 +85,6 @@ class Sequencer:
         for step in range(len(self.sequence)):
             UI.listWidget.item(step).setText(str(step+1))
 
-    def play_sequence(self):
-        if not UI.pushButton_main_sequencerplay.isChecked():
-            self.timer.stop()
-            self.play_step = 0
-            self.loop_step = 0
-            self.loop_count = 0
-            UI.pushButton_main_sequencerplay.toggle()
-        else:
-            UI.pushButton_main_sequencerplay.toggle()
-            self.timer = QtCore.QTimer()
-            for steps in range(len(self.sequence)):
-                self.loop_step = 0
-                fade_time = self.sequence[steps]["time"]
-                self.loop_timer.timeout.connect(self.next_step)
-                self.loop_timer.start(fade_time)
-
-
-    def next_step(self):
-        self.play_step = 0
-        fade_time = self.sequence[self.loop_step]["time"]
-        self.loop_count = int(fade_time / 0.1)
-        self.timer.timeout.connect(lambda: self.animate_slider_step(self.loop_step))
-        self.timer.start(100)
-        self.loop_step += 1
-        if self.loop_step >= len(self.sequence)-1:
-            self.loop_timer.stop()
-
-
-    def animate_slider_step(self, step):
-        for slider in range(1, 13):
-            slider_object = getattr(UI, "verticalSlider_a_"+str(slider))
-            if step != 0:
-                begin = self.sequence[step-1]["verticalSlider_a_"+str(slider)]
-            else:
-                begin = 0
-
-            if step != len(self.sequence)-1:
-                end = self.sequence[step]["verticalSlider_a_"+str(slider)]
-            else:
-                end = 0
-
-            gradient = (end-begin)/self.sequence[step]["time"]
-            change_value = int(gradient*(self.play_step*0.1) + begin)
-
-            slider_object.setValue(change_value)
-
-
-        for slider in range(1, 13):
-            slider_object = getattr(UI, "verticalSlider_b_"+str(slider))
-            if step != 0:
-                begin = self.sequence[step-1]["verticalSlider_b_"+str(slider)]
-            else:
-                begin = 0
-
-            if step != len(self.sequence)-1:
-                end = self.sequence[step]["verticalSlider_b_"+str(slider)]
-            else:
-                end = 0
-
-            gradient = (end-begin)/self.sequence[step]["time"]
-            change_value = int(gradient*(self.play_step*0.1) + begin)
-
-            slider_object.setValue(change_value)
-
-        self.play_step += 1
-        if self.play_step >= self.loop_count:
-            self.timer.stop()
-
-
     def click_step(self, item_clicked):
         row = UI.listWidget.currentRow()
         step = self.sequence[row]
@@ -164,13 +95,57 @@ class Sequencer:
             slider_object = getattr(UI, "verticalSlider_b_"+str(slider))
             slider_object.setValue(step["verticalSlider_b_"+str(slider)])
 
+class SequenceThread(QtCore.QThread):
+    def __init__(self, sequencer: Sequencer):
+        QtCore.QThread.__init__(self)
+        self.sequencer = sequencer
+        self.sequence = self.sequencer.sequence
+
+    def run(self):
+        for index, step in enumerate(self.sequence):
+            for update in range(0, step["time"]*10, 1):
+                for slider in range(1, 13):
+                    slider_object_a = getattr(UI, "verticalSlider_a_"+str(slider))
+                    slider_object_b = getattr(UI, "verticalSlider_b_"+str(slider))
+                    if index:
+                        begin_a = step["verticalSlider_a_"+str(slider)]
+                        begin_b = step["verticalSlider_b_"+str(slider)]
+                    else:
+                        begin_a = 0
+                        begin_b = 0
+
+                    if index == len(self.sequence)-1:
+                        end_a = 0
+                        end_b = 0
+                    else:
+                        end_a = self.sequence[index+1]["verticalSlider_a_"+str(slider)]
+                        end_b = self.sequence[index+1]["verticalSlider_b_"+str(slider)]
+
+                    gradient_a = (end_a-begin_a)/step["time"]
+                    gradient_b = (end_b-begin_b)/step["time"]
+                    change_value_a = int(floor((gradient_a*update*0.1)+begin_a))
+                    change_value_b = int(floor((gradient_b*update*0.1)+begin_b))
+                    slider_object_a.setValue(change_value_a)
+                    slider_object_b.setValue(change_value_b)
+                self.msleep(100)
+
 SEQUENCE = Sequencer()
+
+SEQUENCE_THREAD = SequenceThread(SEQUENCE)
+
+def toggle_play():
+    if UI.pushButton_main_sequencerplay.isChecked():
+        SEQUENCE_THREAD = SequenceThread(SEQUENCE)
+        SEQUENCE_THREAD.start()
+    else:
+        SEQUENCE_THREAD.terminate()
+        del SEQUENCE_THREAD
+
+UI.pushButton_main_sequencerplay.clicked.connect(toggle_play)
 
 UI.pushButton_addStep.clicked.connect(SEQUENCE.add_step)
 UI.pushButton_deleteStep.clicked.connect(SEQUENCE.delete_step)
 UI.listWidget.itemClicked.connect(SEQUENCE.click_step)
-
-UI.pushButton_main_sequencerplay.clicked.connect(SEQUENCE.play_sequence)
 
 UI.verticalSlider_a_1.valueChanged.connect(lambda: DESK.updateLight("verticalSlider_a_1"))
 UI.verticalSlider_a_2.valueChanged.connect(lambda: DESK.updateLight("verticalSlider_a_2"))
