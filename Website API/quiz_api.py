0d05253dd01e8a35fd49346a37ce2eb3411ccaf3
mitgobla <ben-dodd@outlook.com>
2018-10-06 12:52:05 +0100

    Vote V2

---
+++ Website API/quiz_api.py
@@ -0,0 +1,71 @@
+import flask
+#from flaskext.mysql import MySQL
+from flask import request
+
+
+DEBUG = True
+APP = flask.Flask(__name__)
+APP.config.from_object(__name__)
+
+# APP.config['MYSQL_DATABASE_USER'] = 'charityquiz'
+# APP.config['MYSQL_DATABASE_PASSWORD'] = 'charityday'
+# APP.config['MYSQL_DATABASE_DB'] = 'charities_day'
+# APP.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+#SQL = MySQL()
+#SQL.init_app(APP)
+
+#SQL_CONNECTION = SQL.connect()
+
+
+TOTAL_VOTES = [0, 0]
+
+
+@APP.route('/votea', methods=['GET', 'POST'])
+def votea():
+    # SQL_CURSOR = SQL_CONNECTION.cursor()
+    # SQL_CURSOR.execute("UPDATE votes SET a = a + 1 WHERE id = 1")
+    # SQL_CURSOR.close()
+    # SQL_CONNECTION.commit()
+    TOTAL_VOTES[0] += 1
+    return flask.render_template("voted_a.html", votes_a=str(TOTAL_VOTES[0]), votes_b=str(TOTAL_VOTES[1]))
+
+
+@APP.route('/voteb', methods=['GET', 'POST'])
+def voteb():
+    # SQL_CURSOR = SQL_CONNECTION.cursor()
+    # SQL_CURSOR.execute("UPDATE votes SET b = b + 1 WHERE id = 1")
+    # SQL_CURSOR.close()
+    # SQL_CONNECTION.commit()
+    TOTAL_VOTES[1] += 1
+    return flask.render_template("voted_b.html", votes_a=str(TOTAL_VOTES[0]), votes_b=str(TOTAL_VOTES[1]))
+
+
+@APP.route('/unvote_a', methods=['GET', 'POST'])
+def unvote_a():
+    TOTAL_VOTES[0] -= 1
+    # SQL_CURSOR = SQL_CONNECTION.cursor()
+    # SQL_CURSOR.execute("UPDATE votes SET a = a - 1 WHERE id = 1")
+    # SQL_CURSOR.close()
+    # SQL_CONNECTION.commit()
+    return flask.redirect(flask.url_for("index"))
+
+
+@APP.route('/unvote_b', methods=['GET', 'POST'])
+def unvote_b():
+    TOTAL_VOTES[1] -= 1
+    # SQL_CURSOR = SQL_CONNECTION.cursor()
+    # SQL_CURSOR.execute("UPDATE votes SET b = b - 1 WHERE id = 1")
+    # SQL_CURSOR.close()
+    # SQL_CONNECTION.commit()
+    return flask.redirect(flask.url_for("index"))
+
+
+@APP.route('/', methods=['GET', 'POST'])
+def index():
+    """Home Page"""
+    return flask.render_template("vote.html")
+
+
+if __name__ == "__main__":
+    APP.run(port=80, debug=DEBUG)
