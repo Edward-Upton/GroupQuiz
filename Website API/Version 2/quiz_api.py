261f35a21bd18e12a1f0a55e824946f04b97f8c8
engiego <ed.upton@outlook.com>
2018-10-07 19:47:29 +0100

    Started Admin Panel and Home Page

--- Website API/Version 2/quiz_api.py
+++ Website API/Version 2/quiz_api.py
@@ -27,29 +27,53 @@ VOTING_READY = False # Need API to toggle voting
 def index():
     """Home Page"""
     # password = request.args.get('password')
-    if 'adminUser' in request.cookies:
+    if 'userAdmin' in request.cookies:
         # Return ADMIN control panel
-        return
+        return flask.redirect(flask.url_for('adminPanel'))
     else:
-        # Return getCode
-        return flask.redirect(flask.url_for('getCode'))
+        return flask.redirect(flask.url_for('normalUser'))
 
 @APP.route('/admin', methods=['GET', 'POST'])
 def admin():
+    if 'userAdmin' in request.cookies:
+        return flask.redirect(flask.url_for('adminPanel'))
+
     passwordFailed = request.args.get('passwordFailed')
+    notLoggedIn = request.args.get('notLoggedIn')
     if passwordFailed:
-        return flask.render_template("admin_login.html", passwordFailed=True)
-    else:
-        return flask.render_template("admin_login.html")
+        return flask.render_template("adminLogin.html", passwordFailed=True)
+    
+    if notLoggedIn:
+        return flask.render_template('adminLogin.html', notLoggedIn=True)
+
+    return flask.render_template("adminLogin.html")
+
+@APP.route('/normalUser', methods=['GET', 'POST'])
+def normalUser():
+    global code_list
+    if 'userCode' in request.cookies:
+        if request.cookies.get('userCode') in code_list:
+            flask.redirect(flask.url_for('home'))
+        else:
+            code_list.append(request.cookies.get('userCode'))
+
+    return flask.redirect(flask.url_for('getCode'))
 
 @APP.route('/getAdmin', methods=['GET', 'POST'])
 def getAdmin():
     inputted_password = request.form['passwordField']
     if inputted_password != "test_password": # Super secure password, btw
         return flask.redirect(flask.url_for('admin', passwordFailed=True))
+    response = flask.make_response(flask.redirect(flask.url_for('adminPanel')))
+    response.set_cookie('userAdmin', '1')
+    return response
 
-    return "SUCC" # Admin Panel
+@APP.route('/adminPanel', methods=['GET', 'POST'])
+def adminPanel():
+    if not 'userAdmin' in request.cookies:
+        return flask.redirect(flask.url_for('admin', notLoggedIn=True))
 
+    return flask.render_template("adminPanel.html", userCount=str(len(code_list)))
 
 def create_code():
     global code_list
@@ -79,7 +103,7 @@ def getYear():
         return flask.redirect(flask.url_for('getCode'))
 
     if 'userGroup' in request.cookies:
-        return flask.redirect(flask.url_for('panel'))
+        return flask.redirect(flask.url_for('home'))
 
     return flask.render_template('yearForm.html', code=request.cookies.get('userCode'))
 
@@ -89,24 +113,24 @@ def yearForm():
         return flask.redirect(flask.url_for('getCode'))
 
     if 'userGroup' in request.cookies:
-        return flask.redirect(flask.url_for('panel'))
+        return flask.redirect(flask.url_for('home'))
 
     year = request.form['selectYear']
 
-    response = flask.redirect(flask.url_for('panel'))
+    response = flask.redirect(flask.url_for('home'))
     response.set_cookie('userGroup', year)
     return response
 
-@APP.route('/panel', methods=['GET', 'POST'])
-def panel():
+@APP.route('/home', methods=['GET', 'POST'])
+def home():
     if not 'userCode' in request.cookies:
         return flask.redirect(flask.url_for('getCode'))
 
     if not 'userGroup' in request.cookies:
         return flask.redirect(flask.url_for('getYear'))
-
-    return "I LIKE TO HANG OUT WITH MY DOG"
-
+    
+    return flask.render_template('home.html', code=request.cookies.get('userCode'),
+                            year=request.cookies.get("userGroup"))
 
 if __name__ == "__main__":
     APP.run(host="0.0.0.0", port=80, debug=DEBUG)
--- Website API/Version 2/test.py
+++ Website API/Version 2/test.py
@@ -1,4 +1,11 @@
-from numpy import random
+import json
 
-generated_code = random.random()*10000
-print(int(generated_code))
+data = {
+    "quizzes": {
+        "1": {
+            "name": "Embarrasing Question",
+            "questions": ["When did Mr Ward have his first kiss", "Whom'st teacher is Mrs Essex's worsetest teacher?"],
+            "answers": ["Never", "Yeet"]
+        },
+    }
+}
