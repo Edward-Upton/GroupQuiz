306f39ba8d7c5655d29e3a37d9aecdc8ccfeb210
engiego <ed.upton@outlook.com>
2018-10-07 10:39:56 +0100

    quiz_apy.py Rewrite

--- Website API/Version 2/quiz_api.py
+++ Website API/Version 2/quiz_api.py
@@ -1,10 +1,15 @@
 import flask
-from flaskext.mysql import MySQL
+from numpy import random
+#from flaskext.mysql import MySQL
 from flask import request, make_response
+import random
+import string
 
 DEBUG = True
 APP = flask.Flask(__name__)
 APP.config.from_object(__name__)
+
+code_list = list()
 
 VOTES = {
     "7": [0, 0],
@@ -16,57 +21,76 @@ VOTES = {
     "13": [0, 0],
 }
 
-VOTING_READY = False
-
-@APP.route('/toggleVoting', methods=['GET'])
-def toggleVoting():
-    admin_user = request.args.get('username')
-    if admin_user != 'mitgobla':
-        return flask.redirect(flask.url_for('index'))
-    global VOTING_READY
-    if VOTING_READY:
-        VOTING_READY = False
-    else:
-        VOTING_READY = True
-    return "Voting Enabled: "+str(VOTING_READY)
-
-@APP.route('/voted', methods=['POST'])
-def voted():
-    year = request.cookies.get('yearGroup')
-    voteoption = request.form.to_dict(flat=False)
-    if 'vote_a' in voteoption:
-        VOTES[str(year)][0] += 1
-        vote_value = "A"
-    elif 'vote_b' in voteoption:
-        VOTES[str(year)][1] += 1
-        vote_value = "B"
-    response = flask.make_response(flask.render_template('voted.html', vote_option=vote_value))
-    response.set_cookie('yearGroup', year)
-    return response
-
-
-@APP.route('/submitYear', methods=['POST'])
-def submitYear():
-    """Submit Page"""
-    if 'selectYear' in request.form:
-        year = request.form['selectYear']
-        voted = "N"
-    else:
-        year = request.cookies.get('yearGroup')
-        voted = request.cookies.get('voted')
-
-    if VOTING_READY:
-        response = flask.make_response(flask.render_template('vote.html'))
-    else:
-        response = flask.make_response(flask.render_template('vote_notready.html'))
-    response.set_cookie('yearGroup', year)
-    response.set_cookie('voted', voted)
-    return response
+VOTING_READY = False # Need API to toggle voting
 
 @APP.route('/', methods=['GET', 'POST'])
 def index():
     """Home Page"""
-    return flask.render_template("index.html")
+    password = request.args.get('password')
+    if password == 'test': # Definitely a super secure password
+        # Return ADMIN control panel
+        return 
+    else:
+        # Return getCode        
+        return flask.redirect(flask.url_for('getCode'))
+
+def create_code():
+    global code_list
+    code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
+    while code in code_list:
+        code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
+    code_list.append(code)
+    return code
+
+@APP.route('/getCode', methods=['GET', 'POST'])
+def getCode():
+    if 'userCode' in request.cookies:
+        response = flask.make_response(flask.redirect(flask.url_for('getYear')))
+        # response.set_cookie('hasCode', "1")
+        # response.set_cookie('userCode', request.cookies.get('userCode'))
+    else:
+        generated_code = create_code()
+        # response = flask.make_response(flask.render_template("index.html", code=str(generated_code)))
+        response = flask.make_response(flask.redirect(flask.url_for('getYear')))
+        # response.set_cookie('hasCode', "1")
+        response.set_cookie('userCode', generated_code)
+    return response
+
+@APP.route('/getYear', methods=['GET', 'POST'])
+def getYear():
+    if not 'userCode' in request.cookies:
+        return flask.redirect(flask.url_for('getCode'))
+
+    if 'userGroup' in request.cookies:
+        return flask.redirect(flask.url_for('panel'))
+
+    return flask.render_template('yearForm.html', code=request.cookies.get('userCode'))
+        
+@APP.route('/yearForm', methods=['GET', 'POST'])
+def yearForm():
+    if not 'userCode' in request.cookies:
+        return flask.redirect(flask.url_for('getCode'))
+
+    if 'userGroup' in request.cookies:
+        return flask.redirect(flask.url_for('panel'))
+
+    year = request.form['selectYear']
+
+    response = flask.redirect(flask.url_for('panel'))    
+    response.set_cookie('userGroup', year)
+
+    return response
+
+@APP.route('/panel', methods=['GET', 'POST'])
+def panel():
+    if not 'userCode' in request.cookies:
+        return flask.redirect(flask.url_for('getCode'))
+    
+    if not 'userGroup' in request.cookies:
+        return flask.redirect(flask.url_for('getYear'))
+    
+    return "I LIKE TO HANG OUT WITH MY DOG"
+
 
 if __name__ == "__main__":
-    APP.run(host="192.168.1.205", port=80, debug=DEBUG)
+    APP.run(host="0.0.0.0", port=80, debug=DEBUG)
---
+++ Website API/Version 2/test.py
@@ -0,0 +1,4 @@
+from numpy import random
+
+generated_code = random.random()*10000
+print(int(generated_code))
