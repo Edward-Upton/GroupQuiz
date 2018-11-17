e8c9be33ba6c21815e10c47f728aa02a5fc752b7
mitgobla <ben-dodd@outlook.com>
2018-11-17 16:03:35 +0000

    Create GetEGTWinner.sql

---
+++ Database/GetEGTWinner.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(*) FROM `charities_day`.`vote-results` WHERE (`voteName` = 'Emlyns Got Talent' AND voteAnswer = 'Tayla Cadwell');
+SELECT COUNT(*) FROM `charities_day`.`vote-results` WHERE (`voteName` = 'Emlyns Got Talent' AND voteAnswer = 'Cerys & Band');
+SELECT COUNT(*) FROM `charities_day`.`vote-results` WHERE (`voteName` = 'Emlyns Got Talent' AND voteAnswer = 'Cerys & Caitlin');
+SELECT COUNT(*) FROM `charities_day`.`vote-results` WHERE (`voteName` = 'Emlyns Got Talent' AND voteAnswer = 'Tom & Rowan');
+SELECT COUNT(*) FROM `charities_day`.`vote-results` WHERE (`voteName` = 'Emlyns Got Talent' AND voteAnswer = 'Hari & Band');
